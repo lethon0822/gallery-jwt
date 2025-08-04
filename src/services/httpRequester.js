930758e0dcecb4c381ac6114e8709e3f6c3387c4
sbsteacher <id@gmail.com>
2025-08-04 12:35:16 +0900

    axios-interceptors 처리

--- src/services/httpRequester.js
+++ src/services/httpRequester.js
@@ -8,7 +8,16 @@ axios.defaults.baseURL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
 axios.interceptors.response.use( res => res, async err => {
     console.log('err: ', err);
     if(err.response) {
+        const accountStore = useAccountStore();
+        if(err.response.status === 401 && accountStore.isSigned) { //401에러인데 로그인이 되어 있는 상태라면
+            try {
+                await reissue();
 
+                return await axios.request(err.config); //이전에 시도했던 요청으로 다시 요청
+            } catch(e) {
+                accountStore.logout();
+            }
+        }
     }
     return Promise.reject(err);
 });
